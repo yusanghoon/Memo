2b23355a7fdfe8a2e5f6818f58c3b63ffc7cfd02
303-12\user <>
2023-02-07 19:08:48 +0900

    [유상훈] - 시스템 설정

---
+++ src/test/java/com/agora12/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agora12.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
